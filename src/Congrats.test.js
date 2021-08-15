b750632712a5e5487fc9d8f2773bf9e897878bc4
haixin zhang <haixin.zhang.777@gmail.com>
2021-08-15 14:44:43 +1000

    add setup and test utils

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -3,4 +3,16 @@ import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-17-updated";
 import Congrats from "./Congrats";
 
+import { findByTestAttr } from "../test/testUtils";
+
 Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+const setup = (props = {}) => {
+  return shallow(<Congrats {...props} />);
+};
+
+test("renders without errors", () => {});
+
+test("renders no test when `success` props is false", () => {});
+
+test("renders no-empty congrats message when `success` props is true", () => {});
