7a1780735d60ad81dc1f4609508ce4e75ca94b52
haixin zhang <haixin.zhang.777@gmail.com>
2021-08-15 17:22:41 +1000

    add Congrate Component and Unit test

--- src/Congrats.js
+++ src/Congrats.js
@@ -1,7 +1,17 @@
 import React from 'react'
 
-const CongratsComponent = () => {
-	return <div />
+const CongratsComponent = (props) => {
+	if (props.success) {
+		return (
+			<div data-test="component-congrats">
+				<span data-test="component-message">
+					Congratulations! You guessed the world!
+				</span>
+			</div>
+		)
+	} else {
+		return <div data-test="component-congrats" />
+	}
 }
 
 export default CongratsComponent
