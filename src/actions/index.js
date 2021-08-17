3d28a0f5978202cdb95482a2275c7f345d0da619
haixin zhang <haixin.zhang.777@gmail.com>
2021-08-17 23:16:52 +1000

    add thunk for guessedwords piece of state

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import { getLetterMatchCount } from '../helpers'
+
 export const actionTypes = {
 	CORRECT_GUESS: 'CORRECT_GUESS',
 	GUESS_WORD: 'GUESS_WORD',
@@ -8,5 +10,17 @@ export const correctGuess = () => {
 }
 
 export const guessWord = (guessedWord) => {
-	return (dispatch, getState) => {}
+	return (dispatch, getState) => {
+		const secretWord = getState().secretWord
+		const letterMatchCount = getLetterMatchCount(guessedWord, secretWord)
+
+		dispatch({
+			type: actionTypes.GUESS_WORD,
+			payload: { guessedWord, letterMatchCount },
+		})
+
+		if (guessedWord === secretWord) {
+			dispatch({ type: actionTypes.CORRECT_GUESS })
+		}
+	}
 }
