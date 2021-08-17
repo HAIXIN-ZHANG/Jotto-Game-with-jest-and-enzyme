1f594e2e4977b31e6a3949d481fb39af27097419
haixin zhang <haixin.zhang.777@gmail.com>
2021-08-17 20:25:28 +1000

    Input component render tests for success: false

--- src/Input.js
+++ src/Input.js
@@ -3,11 +3,25 @@ import { connect } from 'react-redux'
 
 class Input extends React.Component {
 	render() {
-		return (
-			<div>
-				<button />
-			</div>
+		const contents = this.props.success ? null : (
+			<form className="form-inline">
+				<input
+					data-test="input-box"
+					className="mb-2 mx-sm-3"
+					id="word-guess"
+					type="text"
+					placeholder="enter guess"
+				/>
+				<button
+					data-test="submit-button"
+					className="btn btn-primary mb-2"
+					type="submit"
+					Submit
+				/>
+			</form>
 		)
+
+		return <div data-test="component-input">{contents}</div>
 	}
 }
 
